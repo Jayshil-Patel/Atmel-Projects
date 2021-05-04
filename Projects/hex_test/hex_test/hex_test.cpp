b4821c96efa2a6856a648a7f074253155d9587e9
Jayshil Patel <78102199+Jayshil-Patel@users.noreply.github.com>
2021-05-04 14:43:18 +0530

    Add files via upload

---
+++ Projects/hex_test/hex_test/hex_test.cpp
@@ -0,0 +1,19 @@
+/*
+ * hex_test.cpp
+ *
+ * Created: 02-10-2020 20:56:28
+ *  Author: Jay
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+	DDRC=0xff;
+    while(1)
+    {
+		PORTC=0xff;
+        //TODO:: Please write your application code 
+    }
+}
